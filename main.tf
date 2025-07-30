ed8c26b11036b10fd884fe7c30c0367e2d40e02c
Jhonatan <jmonzonllanos@gmail.com>
2025-07-30 15:21:53 -0500

    test modules token https

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
-module "s3_bucket" {
-  source = "git@github.com:sijnal/tf-modules.git//aws/s3?ref=main"
-  bucket_name = "mi-bucket-simple-sijnal"
+module "s3_bucket-v0" {
+  source = "https://github.com/sijnal/poc-tf-modules.git//s3?ref=vpc-v1.0.0"
+  bucket = "modules-s3"
 }
+
+module "s3_bucket-v3" {
+  source = "https://github.com/sijnal/poc-tf-modules.git//s3?ref=s3-v3.0.0"
+  bucket = "poc-tf-modules-s3"
+}
+
+module "s3_bucket-v4" {
+  source = "https://github.com/sijnal/poc-tf-modules.git//s3?ref=s3-v4.0.0"
+  bucket = "tf-modules-s3"
+}
+
