1bf416692b932aa5e25bedc746c923c94b1d621a
Jhonatan <jmonzonllanos@gmail.com>
2025-08-05 10:45:05 -0500

    test deploy keys v5.0.0

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-# module "s3_bucket-v0" {
-#   source = "git@github.com:sijnal/poc-tf-modules.git//s3?ref=vpc-v1.0.0"
-#   bucket = "modules-s3"
+module "s3_bucket-v0" {
+  source = "git@github.com:sijnal/poc-tf-modules.git//s3?ref=vpc-v1.0.0"
+  bucket = "modules-s3"
+}
+
+# module "s3_bucket-v3" {
+#   source = "git@github.com:sijnal/poc-tf-modules.git//s3?ref=s3-v3.0.0"
+#   bucket = "poc-tf-modules-s3"
 # }
 
-module "s3_bucket-v3" {
-  source = "git@github.com:sijnal/poc-tf-modules.git//s3?ref=s3-v3.0.0"
-  bucket = "poc-tf-modules-s3"
-}
-
-module "s3_bucket-v4" {
-  source = "git@github.com:sijnal/poc-tf-modules.git//s3?ref=s3-v4.0.0"
-  bucket = "poc-ssh-modules-s3"
-}
+# module "s3_bucket-v4" {
+#   source = "git@github.com:sijnal/poc-tf-modules.git//s3?ref=s3-v4.0.0"
+#   bucket = "poc-ssh-modules-s3"
+# }
 
 # module "github_actions_oidc" {
 #   source = "./modules/github-actions-oidc"
