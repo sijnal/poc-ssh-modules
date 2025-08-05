8965f1f0ad3249922e631ca2448057b4774294da
Jhonatan <jmonzonllanos@gmail.com>
2025-08-05 11:10:04 -0500

    test deploy keys v5.0.3

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-module "s3_bucket-v0" {
-  source = "./modules/s3"
-  bucket = "modules-s3-test-1.1"
+# module "s3_bucket-v0" {
+#   source = "./modules/s3"
+#   bucket = "modules-s3-test-1.1"
+# }
+
+module "s3_bucket-v3" {
+  source = "./module/s3"
+  bucket = "module-s3-test-2"
 }
 
-# module "s3_bucket-v3" {
-#   source = "./module/s3"
-#   bucket = "module-s3-test-2"
-# }
-
-# module "s3_bucket-v4" {
-#   source = "./module/s3"
-#   bucket = "module-s3-test-3"
-# }
+module "s3_bucket-v4" {
+  source = "./module/s3"
+  bucket = "module-s3-test-3"
+}
 
 module "github_actions_oidc" {
   source = "./modules/github-actions-oidc"
