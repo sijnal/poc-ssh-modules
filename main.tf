6d573bf1758dec30837f4c1f621777a0d0ed23c6
Jhonatan <jmonzonllanos@gmail.com>
2025-08-05 11:07:41 -0500

    test deploy keys v5.0.2

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 module "s3_bucket-v0" {
   source = "./modules/s3"
-  bucket = "modules-s3-test-1"
+  bucket = "modules-s3-test-1.1"
 }
 
 # module "s3_bucket-v3" {
-#   source = "./modules/s3"
-#   bucket = "modules-s3-test-2"
+#   source = "./module/s3"
+#   bucket = "module-s3-test-2"
 # }
 
 # module "s3_bucket-v4" {
-#   source = "./modules/s3"
-#   bucket = "modules-s3-test-3"
+#   source = "./module/s3"
+#   bucket = "module-s3-test-3"
 # }
 
-# module "github_actions_oidc" {
-#   source = "./modules/github-actions-oidc"
+module "github_actions_oidc" {
+  source = "./modules/github-actions-oidc"
 
-#   role_name                   = "tos-dev-role-github"
-#   organization                = "sijnal"
-#   repository                  = "poc-ssh-modules"
-#   branch                      = "main"
-#   attach_administrator_policy = true
-#   create_oidc_provider        = true
-# }
+  role_name                   = "tos-prod-role-github"
+  organization                = "sijnal"
+  repository                  = "poc-ssh-modules"
+  branch                      = "main"
+  attach_administrator_policy = true
+  create_oidc_provider        = false
+}
